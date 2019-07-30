01dddfe0bd842dff05950d09d1799233d41b5bd4
sunyixin <sunyixin@zrtec.net>
2019-07-30 14:39:44 +0800

    first commit

---
+++ src/JunitServiceProvide.php
@@ -0,0 +1,31 @@
+<?php
+namespace Syx\JunitLaravel;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class JunitServiceProvide extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->registerRoutes();
+
+        $this->loadViewsFrom(
+            __DIR__.'/../resources/views', 'junit'
+        );
+    }
+
+    private function routeConfiguration()
+    {
+        return [
+            'namespace' => 'Syx\JunitLaravel\Http\Controllers',
+            'prefix' => 'junit',
+        ];
+    }
+    private function registerRoutes()
+    {
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/Http/routes.php');
+        });
+    }
+}
